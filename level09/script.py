b19b9806dbfb1aa98e6c59fcdb816d37826cbbe2
kuriiliin <kasmiabdoullah09@icloud.com>
2023-12-24 19:42:28 +0100

    level09 finished

--- level09/script.py
+++ level09/script.py
@@ -1,2 +1,3 @@
-str = list(open('token', 'r'))[0]
-flag = ''.join([chr(ord(i) - str.find(i)) for i in str])
+string = enumerate(list(open('/home/user/level09/token', 'r'))[0].rstrip('\n'))
+flag = ''.join([chr(ord(i) - c) for c, i in string])
+print('flag : ' + flag + ' | with len ' + str(len(flag)))
